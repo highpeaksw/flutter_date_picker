a7299c30f149964807ad0572f460b996ee307ddb
Rajesh Kumar <rajeshkumar@HPS184.local>
2018-07-22 14:20:06 +0530

    test

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:flutter_cupertino_date_picker/date_picker.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
